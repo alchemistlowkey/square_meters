a1997350cd1e3b580c2d08e71d9c271d7ae342d7
alchemistlowkey <alchemistlowkey@gmail.com>
2024-08-14 21:17:04 +0100

    renamed to Footers

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import "../global.css";
   import "../index.css";  
   import WaIcon from "../components/WaIcon.svelte";
-  import Footer from "../components/Footer.svelte";
+  import Footers from "../components/Footers.svelte";
   import Nav from "../components/Nav.svelte";
 </script>
 
@@ -14,6 +14,6 @@
 
 <WaIcon />
 
-<Footer />
+<Footers />
 
 <style></style>

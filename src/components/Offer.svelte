f3ec60ab799f252990db0b00fc23576131457d69
alchemistlowkey <alchemistlowkey@gmail.com>
2024-08-15 00:14:04 +0100

    hompage fixed

--- src/components/Offer.svelte
+++ src/components/Offer.svelte
@@ -1,14 +1,49 @@
-<div class="container my-lg-5 pt-3">
-  <div class="offer-cite">
-    <div class="">
-      <div class="first">Experience SquareMetres Now</div>
+<div class="container pt-3">
+  <div class="row offer-cite">
+    <div class="col ps-4">
+      <div class="first text-nowrap">Experience SquareMetres Now</div>
 
-      <div class="second">Start Investing Today</div>
+      <div class="second text-center pt-3">Start Investing Today</div>
     </div>
-    <div class="">&nbsp;</div>
-
-    <div class="">
+    <div class="col mt-md-5 mt-5 mt-lg-0">
       <a class="offer-cite-btn btn btn-danger" href="/"> Our Offering </a>
     </div>
   </div>
 </div>
+
+<style>
+  .offer-cite {
+    background-color: #f8f9fa;
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+  .offer-cite .first {
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #bbb;
+  }
+
+  .offer-cite .second {
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #fff;
+  }
+
+  .offer-cite-btn {
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: #fff;
+    border: none;
+    border-radius: 10px;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  .offer-cite-btn:hover {
+    background-color: #ff0000;
+  }
+</style>

9fe7f8555bb04e1c0f9c50ca39d67cd3c4c28da6
alchemistlowkey <alchemistlowkey@gmail.com>
2024-08-17 18:25:30 +0100

    career page partly rendered

--- src/components/Career.svelte
+++ src/components/Career.svelte
@@ -5,16 +5,16 @@
   <div class="container">
     <div class="row mx-0 py-16">
       <div class="col-12 col-md-10 py-10 mx-auto text-center">
-        <p class="career text-w">Careers at SQUARE METRES</p>
-        <h1 class="text-w privacy-data">
-            A Successful team is a group of many hand and one mind.
+        <p class="career text-w pb-3">Careers at SQUARE METRES</p>
+        <h1 class="text-w privacy-data py-4">
+          A Successful team is a group of many hand and one mind.
         </h1>
-        <p class="text-w p-content fs-md-6">
-            We've assembled an exceptional team of forward thinkers, fueled by
-            passion and wielding skills that surpasses the ordinary.
+        <p class="text-w p-content fs-md-6 pb-4">
+          We've assembled an exceptional team of forward thinkers, fueled by
+          passion and wielding skills that surpasses the ordinary.
         </p>
-        <button class="btn privacy-btn mt-3">
-          <a href="/contact" target="_blank"> Contact Us </a>
+        <button class="btn privacy-btn text-w">
+          <a href="/contact" target="_blank">All Open Roles</a>
         </button>
       </div>
     </div>
@@ -23,173 +23,67 @@
 
 <section class="py-10 mb-16">
   <div class="container">
-    <div class="row mx-0 py-10 p-text text-cgreen">
-      <div class="collection">Collection of Information</div>
-      <p>We receive and store information about you such as:</p>
-      <div class="">
-        <ul class="responsive-list">
-          <li>
-            <span class="boldtext">Information you provide to us:</span>
-            We collect information you provide to us, which includes:
-            <ul class="responsive-list">
-              <li>
-                your name, email address, telephone number, and other
-                identifiers you might use. We collect this information in a
-                number of ways, including when you enter it while using our
-                website, interact with our customer service, or participate in
-                surveys or marketing promotions;
-              </li>
-            </ul>
-          </li>
-          <li>
-            <span class="boldtext">Information we collect automatically:</span>
-            We collect information about you and your use of our service, your interactions
-            with us and our advertising, as well as information regarding your network,
-            network devices, and your computer you might use to access our website.
-            This information includes:
-            <ul class="responsive-list">
-              <li>
-                your interactions with our emails and texts, and with our
-                messages through push and online messaging channels;
-              </li>
-              <li>
-                details of your interactions with our customer service, such as
-                the date, time, and reason for contacting us, transcripts of any
-                chat conversations, and if you call us, your phone number and
-                call recordings;
-              </li>
-              <li>
-                re-settable device identifiers (also known as advertising
-                identifiers), such as those on mobile devices, tablets, and
-                streaming media devices that include such identifiers;
-              </li>
-              <li>
-                information collected via the use of cookies, web beacons, and
-                other technologies, including ad information (such as
-                information on the availability and delivery of ads, the site
-                URL, as well as the date and time).
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-      <div class="py-4 mx-0">
-        <h6 class="boldtext">What we do with your information</h6>
-        <p>
-          We use information to provide, analyze, administer, enhance, and
-          personalize our services and marketing efforts, to process your
-          registration, your orders, and your payments, and to communicate with
-          you on these and other topics. For example, we use such information
-          to:
-        </p>
-        <div class="py-3 mx-0">
-          <p>We may use your personal information to:</p>
+    <div class="row mx-0 py-10 p-text text-cgreen text-center">
+      <div class="collection text-center">A little about working with us</div>
+      <p class="py-2">
+        At Square Meters, we prioritize the well-being of our team because it's
+        not only the right thing to do, as it is what empowers us to deliver our
+        best work.
+      </p>
 
-          <ul class="responsive-list">
-            <li>
-              help us quickly and efficiently respond to inquiries and requests;
-            </li>
-            <li>
-              provide information on our products and services – we use
-              information to provide you with information on products and
-              services you have requested, and to communicate with you about
-              them;
-            </li>
-            <li>
-              communicate promotions and competitions – we use contact details
-              you have given us, to provide you with information about our
-              promotions, offers, and events that we think will be of interest
-              to you;
-            </li>
-            <li>
-              respond and process enquiries, suggestions, and complaints; if you
-              make an enquiry, suggestion, or complaint, we will use that
-              information to provide you with an appropriate solution and to
-              improve our products, services, and communications.
-            </li>
-          </ul>
-        </div>
+      <p class="py-2">
+        Are you beginning your career or already a star, our system provides
+        numerous opportunities for you to make a positive impact on people's
+        lives.
+      </p>
+      <p class="py-2">
+        SquareMetres is a welcoming place for everyone, and we do our best to
+        make sure our people feel supported and happy at work.
+      </p>
+    </div>
+
+    <div class="row mx-0 py-10 p-text text-cgreen text-center">
+      <div class="collection text-center">Life at Square Metres</div>
+      <p class="py-2">
+        In everything we do, we uphold these values without compromise. At
+        Square Meters, its people over process and teamwork over perfection.
+      </p>
+    </div>
+
+    <div class="row mx-0 py-10 p-text text-cgreen text-center">
+      <div class="col-4 text-center">
+        <img src="/images/goal.png" alt="" class="mx-auto d-block" />
+        <p class="py-4 fw-bold">Find your Balance</p>
+        <p>
+          We recognize the importance of work-life balance. encourage
+          collaboration & nurture career growth
+        </p>
       </div>
-    </div>
-    <div class="row mx-0 p-text py-3 text-cgreen">
-      <div class="collection">When do we send you marketing communications</div>
-      <p>
-        We will only send you marketing communications where there is a
-        necessity for us to do so. You have the right to withdraw your consent
-        for us to use your personal information at any time for marketing
-        communications. To unsubscribe from marketing communications, click on
-        the ‘unsubscribe’ link in any marketing email received from us.
-      </p>
-    </div>
-    <div class="row mx-0 p-text py-3 text-cgreen">
-      <div class="collection">
-        What is our legal basis for using your personal information
+      <div class="col-4 text-center">
+        <img src="/images/growth.png" alt="" class="mx-auto d-block" />
+        <p class="py-4 fw-bold">A Place For All</p>
+        <p>
+          Diversity and inclusion are at the core of our beingnetfl. There's a
+          space for everyone at Square Metres
+        </p>
       </div>
-      <p>
-        We only use your personal information where it is permitted by laws that
-        protect your privacy rights. These include but not are not limited to:
-      </p>
-      <ul class="responsive-list">
-        <li>we have your consent (if applicable);</li>
-        <li>we need to use the information to perform a contract with you;</li>
-        <li>
-          it is fair to use the personal information in our interests and at no
-          disadvantage whatsoever to your personal interests– this can include
-          but not limited to marketing additional products or services to you.
-        </li>
-      </ul>
-    </div>
-    <div class="row mx-0 p-text py-3 text-cgreen">
-      <div class="collection">Retention of Your Personal Data</div>
-      <p>
-        The Company will retain Your Personal Data only for as long as is
-        necessary for the purposes set out in this Privacy Policy. We will
-        retain and use Your Personal Data to the extent necessary to comply with
-        our legal obligations (for example, if we are required to retain your
-        data to comply with applicable laws), resolve disputes, and enforce our
-        legal agreements and policies. The Company will also retain Usage Data
-        for internal analysis purposes. Usage Data is generally retained for a
-        shorter period of time, except when this data is used to strengthen the
-        security or to improve the functionality of Our Service, or We are
-        legally obligated to retain this data for longer time periods.
-      </p>
-    </div>
-    <div class="row mx-0 p-text py-3 text-cgreen">
-      <div class="collection">Other legal requirements</div>
-      <p>
-        The Company may disclose Your Personal Data in the good faith belief
-        that such action is necessary to:
-      </p>
-      <ul class="responsive-list">
-        <li>Comply with a legal obligation</li>
-        <li>Protect and defend the rights or property of the Company</li>
-        <li>
-          Prevent or investigate possible wrongdoing in connection with the
-          Service
-        </li>
-        <li>
-          Protect the personal safety of Users of the Service or the public
-        </li>
-        <li>Protect against legal liability</li>
-      </ul>
-    </div>
-    <div class="row mx-0 p-text py-3 text-cgreen">
-      <div class="collection">Information Security</div>
-      <p>
-        The security of Your Personal Data is important to Us, but remember that
-        no method of transmission over the Internet, or method of electronic
-        storage is 100% secure. While We strive to use commercially acceptable
-        means to protect Your Personal Data, We cannot guarantee its absolute
-        security.
-      </p>
+      <div class="col-4 text-center">
+        <img src="/images/core-values.png" alt="" class="mx-auto d-block" />
+        <p class="py-4 fw-bold">
+            Thrive With Us
+            </p>
+        <p>
+            Join us on a journey of growth and learning. Engage in personalized training, unwind at happy hours, and more ....
+        </p>
+      </div>
     </div>
   </div>
 </section>
 
 <style>
-    section{
-        font-family: Poppins;
-    }
+  section {
+    font-family: Poppins;
+  }
   .career-bg {
     background-image: linear-gradient(#0000009e, #001410),
       url("../lib/img/career.jpeg");
@@ -230,44 +124,13 @@
     text-align: left;
     color: #f96b29;
   }
-  .boldtext {
-    font-size: 25px;
-    font-weight: 700;
-    line-height: 37.5px;
-    text-align: left;
-  }
+
   .p-text {
-    font-size: 20px;
+    font-size: 16px;
     font-weight: 400;
-    line-height: 37.5px;
     text-align: left;
   }
-  .responsive-list {
-    list-style-type: disc;
-    margin-left: 1.5rem;
-  }
-  .responsive-list ul {
-    list-style-type: circle;
-    margin-left: 1.5rem;
-  }
 
-  @media (max-width: 768px) {
-    .responsive-list {
-      margin-left: 1rem;
-    }
-    .responsive-list ul {
-      margin-left: 1rem;
-    }
-  }
-
-  @media (max-width: 576px) {
-    .responsive-list {
-      margin-left: 0.5rem;
-    }
-    .responsive-list ul {
-      margin-left: 0.5rem;
-    }
-  }
   .text-cgreen {
     color: #0d493d;
   }

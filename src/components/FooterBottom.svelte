ebf48fb248112317b3bcd2c254eaf00cca714038
alchemistlowkey <alchemistlowkey@gmail.com>
2024-08-23 18:08:30 +0100

    interchanged the footer tags

--- src/components/FooterBottom.svelte
+++ src/components/FooterBottom.svelte
@@ -1,16 +1,8 @@
 <section class="footer-bottom border-top mt-5 pt-3 pb-5">
   <div class="container">
     <div class="row text-green fs-5">
-      <div class="col-6 col-md-6 fs-6 float-start fw-semibold px-sm-0 px-md-4 px-1 pt-3">
-        <p class="" data-wow-duration="1s">
-          &copy;2024 Squaremetres.
-        </p>
-        <p class="" data-wow-duration="4s">
-          All rights reserved
-        </p>
-      </div>
-      <div class="col-6 col-md-6 float-end pt-3">
-        <ul class="d-flex float-end">
+      <div class="col-6 col-md-6 float-start pt-3">
+        <ul class="d-flex">
           <li class="px-2 px-sm-3 px-md-4">
             <a href="https://www.instagram.com/squaremetresng/" target="_blank"
               ><i class="bi-instagram"></i></a
@@ -38,6 +30,12 @@
             >
           </li>
         </ul>
+      </div>
+      <div class="col-6 col-md-6 fs-6 float-end fw-semibold px-sm-0 pt-3">
+        <div class="float-end md:pr-[120px] pr-[40px]">
+          <p class="" data-wow-duration="1s">&copy; 2024 Squaremetres.</p>
+          <p class="" data-wow-duration="4s">All rights reserved</p>
+        </div>
       </div>
     </div>
   </div>

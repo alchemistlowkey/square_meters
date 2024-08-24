243c890322e7304c4c0ec48c5983f9b8e8ed590b
alchemistlowkey <alchemistlowkey@gmail.com>
2024-08-24 21:30:30 +0100

    product page rendered

--- src/components/Product.svelte
+++ src/components/Product.svelte
@@ -9,9 +9,34 @@
   CardHeader="Our Products"
     CardContent="You are a step closer to making a great decision today, cheers."
   >
-    <Button btnDetails="See our projects"></Button>
+    <Button btnDetails="See our projects"
+    btnlink="#projects"
+    ></Button>
   </PageHead>
 </Bg>
+
+<section id="projects">
+  <div class="container mt-4">
+  <div class="w-100">
+    <div class="row mx-0">
+      <div class="col-md-6 mb-sm-1">
+        <div class="prod-land2 justify-content-start ps-5">
+          <p class="fw-normal text-xs">Epe, Lagos state</p>
+          <h5 class="text-4xl">
+            Fitila Signatures
+          </h5>
+        </div>
+      </div>
+      <div class="col-md-6 mb-sm-1">
+        <div class="prod-land justify-content-start ps-5">
+          <p class="fw-normal text-xs">Asaba, Delta State</p>
+          <h5 class="text-4xl">Dunika Land</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+</section>
 
 <style>
 </style>

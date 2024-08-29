56ca5600d71237913ac6102703a13c9ca3d28b51
alchemistlowkey <alchemistlowkey@gmail.com>
2024-08-29 07:19:04 +0100

    meta tracking removed

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			// default options are shown
-			// out: 'build'
+			out: 'build'
 			// precompress: true,
 			// env: {
 			// 	host: 'HOST',

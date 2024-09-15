6e6bb721ae83cbc1d7507a413878516df566e62b
alchemistlowkey <alchemistlowkey@gmail.com>
2024-09-15 21:51:41 +0100

    frontend: duplicated canonical fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -71,7 +71,6 @@
     document.head.innerHTML += `
       <meta name="description" content="Square Metres Construction Limited is more than just a real estate development company, we are passionate about driving a wholesome experience through real estate and investment solutions. Our Journey began with a simple yet profound goal which is to provide investors with an experience that truly reflects the potential of their real estate investment">
 
-      <link rel="canonical" href="https://squaremetres.ng/">
       <!-- Add more meta tags as needed -->
     `;
   });

d88b2a470280890670b0f434986d35cffb95134e
alchemistlowkey <alchemistlowkey@gmail.com>
2024-09-15 22:38:29 +0100

    frontend: vercel analytics and speed insights deployed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,10 @@
     };
   };
 
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
+
   import { onMount } from "svelte";
   import "../bg.css";
   import "../custom.css";
@@ -19,6 +23,10 @@
   import Footers from "../components/Footers.svelte";
   import NavBar from "../components/NavBar.svelte";
   import Nav from "../components/Nav.svelte";
+
+  injectSpeedInsights();
+
+  inject({ mode: dev ? "development" : "production" });
 
   let description =
     "Square Metres Construction Limited is more than just a real estate development company, we are passionate about driving a wholesome experience through real estate and investment solutions. Our Journey began with a simple yet profound goal which is to provide investors with an experience that truly reflects the potential of their real estate investment";
@@ -94,27 +102,27 @@
     property="twitter:image"
     content="https://squaremetres.ng/favicon.png"
   />
-    <meta name="twitter:site" content="@squaremetresng" />
-    <meta name="twitter:creator" content="@squaremetresng" />
-    <meta name="twitter:domain" content="https://squaremetres.ng" />
-    <meta name="twitter:label1" content="Written by" />
-    <meta name="twitter:data1" content="Square Metres" />
-    <meta name="twitter:label2" content="Filed under" />
-    <meta name="twitter:data2" content="Real Estate" />
-    <meta name="twitter:label3" content="Filed under" />
-    <meta name="twitter:data3" content="Construction" />
-    <meta name="twitter:label4" content="Filed under" />
-    <meta name="twitter:data4" content="Property" />
-    <meta name="twitter:label5" content="Filed under" />
-    <meta name="twitter:data5" content="Investment" />
-    <meta name="twitter:label6" content="Filed under" />
-    <meta name="twitter:data6" content="Land" />
-    <meta name="twitter:label7" content="Filed under" />
-    <meta name="twitter:data7" content="House" />
-    <meta name="twitter:label8" content="Filed under" />
-    <meta name="twitter:data8" content="Home" />
-    <meta name="twitter:label9" content="Filed under" />
-    <meta name="twitter:data9" content="Apartment" />
+  <meta name="twitter:site" content="@squaremetresng" />
+  <meta name="twitter:creator" content="@squaremetresng" />
+  <meta name="twitter:domain" content="https://squaremetres.ng" />
+  <meta name="twitter:label1" content="Written by" />
+  <meta name="twitter:data1" content="Square Metres" />
+  <meta name="twitter:label2" content="Filed under" />
+  <meta name="twitter:data2" content="Real Estate" />
+  <meta name="twitter:label3" content="Filed under" />
+  <meta name="twitter:data3" content="Construction" />
+  <meta name="twitter:label4" content="Filed under" />
+  <meta name="twitter:data4" content="Property" />
+  <meta name="twitter:label5" content="Filed under" />
+  <meta name="twitter:data5" content="Investment" />
+  <meta name="twitter:label6" content="Filed under" />
+  <meta name="twitter:data6" content="Land" />
+  <meta name="twitter:label7" content="Filed under" />
+  <meta name="twitter:data7" content="House" />
+  <meta name="twitter:label8" content="Filed under" />
+  <meta name="twitter:data8" content="Home" />
+  <meta name="twitter:label9" content="Filed under" />
+  <meta name="twitter:data9" content="Apartment" />
 </svelte:head>
 
 <noscript>

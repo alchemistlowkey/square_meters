eff317e2729d20fe08f633e3fcb8bfd60434a1d3
alchemistlowkey <alchemistlowkey@gmail.com>
2024-09-15 05:07:43 +0100

    frontend: SEO work in progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,40 @@
 
 <svelte:head>
     <title>{title}</title>
-    <link rel="canonical" href="https://squaremetres.ng" />
+    <link rel="canonical" href="https://squaremetres.ng/" />
     <meta name="description" content="Square Metres" />
+    <meta name="keywords" content="Real Estate, Investment, Construction, Property, Land, House, Home, Apartment, Square Metres, Square Metres Construction Limited, Square Metres Construction, Square Metres Nigeria, Square Metres NG, Square Metres Limited, Square Metres NG Limited, Square Metres Nigeria Limited, Square Metres Construction Nigeria, Square Metres Construction" />
+    <meta name="image" content="https://squaremetres.ng/favicon.png" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content="Square Metres Construction Limited is more than just a real estate development company, we are passionate about driving a wholesome experience through real estate and investment solutions. Our Journey began with a simple yet profound goal which is to provide investors with an experience that truly reflects the potential of their real estate investment" />
+    <meta property="og:url" content="https://squaremetres.ng/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="https://squaremetres.ng/favicon.png" />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={title} />
+    <meta name="twitter:description" content="Square Metres Construction Limited is more than just a real estate development company, we are passionate about driving a wholesome experience through real estate and investment solutions. Our Journey began with a simple yet profound goal which is to provide investors with an experience that truly reflects the potential of their real estate investment" />
+    <meta name="twitter:image" content="https://squaremetres.ng/favicon.png" />
+    <meta name="twitter:site" content="@squaremetresng" />
+    <meta name="twitter:creator" content="@squaremetresng" />
+    <meta name="twitter:domain" content="https://squaremetres.ng" />
+    <meta name="twitter:label1" content="Written by" />
+    <meta name="twitter:data1" content="Square Metres" />
+    <meta name="twitter:label2" content="Filed under" />
+    <meta name="twitter:data2" content="Real Estate" />
+    <meta name="twitter:label3" content="Filed under" />
+    <meta name="twitter:data3" content="Construction" />
+    <meta name="twitter:label4" content="Filed under" />
+    <meta name="twitter:data4" content="Property" />
+    <meta name="twitter:label5" content="Filed under" />
+    <meta name="twitter:data5" content="Investment" />
+    <meta name="twitter:label6" content="Filed under" />
+    <meta name="twitter:data6" content="Land" />
+    <meta name="twitter:label7" content="Filed under" />
+    <meta name="twitter:data7" content="House" />
+    <meta name="twitter:label8" content="Filed under" />
+    <meta name="twitter:data8" content="Home" />
+    <meta name="twitter:label9" content="Filed under" />
+    <meta name="twitter:data9" content="Apartment" />
 </svelte:head>
 
 <Home />

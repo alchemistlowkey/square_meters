68cf2390b32e7ba4d5350b8a8dc5002bbed52296
alchemistlowkey <alchemistlowkey@gmail.com>
2024-09-16 22:47:50 +0100

    frontend: product page added to sitemap

--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -5,6 +5,11 @@
 
 <svelte:head>
     <title>{title}</title>
+    <link rel="canonical" href="https://squaremetres.ng/product" />
+    <meta property="og:title" content={title} />
+    <meta property="og:description" content="Product Page" />
+    <meta property="og:url" content="https://squaremetres.ng/product" />
+    <meta property="og:type" content="website" />
 </svelte:head>
 
 <Product />

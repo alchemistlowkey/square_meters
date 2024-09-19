ed3189a2c34fceae61436f8892e3c34310633fa9
alchemistlowkey <alchemistlowkey@gmail.com>
2024-09-19 08:26:08 +0100

    backend: email notification setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-    import Home from "../components/Home.svelte";
-    export let title = "Square Metres - Affordable Land Solutions";
+  import Home from "../components/Home.svelte";
+  export let title = "Square Metres - Affordable Land Solutions";
 </script>
 
 <svelte:head>
-    <title>{title}</title>
-    <link rel="canonical" href="https://squaremetres.ng" />
+  <title>{title}</title>
+  <link rel="canonical" href="https://squaremetres.ng" />
 </svelte:head>
 
 <Home />

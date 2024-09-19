188c8bb550bd1ad8de0d791158a93a50987b4283
alchemistlowkey <alchemistlowkey@gmail.com>
2024-09-19 01:33:16 +0100

    frontend: modified footer text and divs

--- src/components/FooterBottom.svelte
+++ src/components/FooterBottom.svelte
@@ -2,44 +2,42 @@
   import FooterSocial from "../shared/FooterSocial.svelte";
 </script>
 
-<section
-  class="footer-bottom mt-5 pb-5 md:text-base lg:text-lg text-sm w-[90%] mx-auto"
+<div
+  class="container mt-5 pb-5 md:text-base lg:text-lg text-sm xl:w-[95%] w-[100%] px-0 mx-auto"
 >
-  <div class="container">
-    <div class="row mx-0 text-[#0D493D]">
-      <FooterSocial
-        SocialItem={[
-          {
-            href: "https://www.instagram.com/squaremetresng/",
-            icon: "bi-instagram",
-          },
-          {
-            href: "https://web.facebook.com/squaremetresng",
-            icon: "bi-facebook",
-          },
-          {
-            href: "https://www.linkedin.com/company/square-meters-real-estate-limited?trk=feed_main-feed-card_feed-actor-name",
-            icon: "bi-linkedin",
-          },
-          { href: "https://x.com/squaremetresng", icon: "bi-twitter" },
-          {
-            href: "https://www.youtube.com/@squaremetresng",
-            icon: "bi-youtube",
-          },
-        ]}
-      />
-      <div
-        class="col-6 col-md-6 text-[10px] md:text-base lg:text-lg float-end fw-medium px-sm-0 pt-3"
-      >
-        <div class="float-end md:pr-[120px] sm:pr-[40px] pe-0">
-          <p class="md:text-nowrap" data-wow-duration="1s">
-            &copy; 2024 Squaremetres. All rights reserved
-          </p>
-        </div>
+  <div class="row mx-0 text-[#0D493D]">
+    <FooterSocial
+      SocialItem={[
+        {
+          href: "https://www.instagram.com/squaremetresng/",
+          icon: "bi-instagram",
+        },
+        {
+          href: "https://web.facebook.com/squaremetresng",
+          icon: "bi-facebook",
+        },
+        {
+          href: "https://www.linkedin.com/company/square-meters-real-estate-limited?trk=feed_main-feed-card_feed-actor-name",
+          icon: "bi-linkedin",
+        },
+        { href: "https://x.com/squaremetresng", icon: "bi-twitter" },
+        {
+          href: "https://www.youtube.com/@squaremetresng",
+          icon: "bi-youtube",
+        },
+      ]}
+    />
+    <div
+      class="col-6 col-md-6 text-[10px] md:text-base lg:text-lg float-end fw-medium px-sm-0 pt-3"
+    >
+      <div class="float-end md:pr-[120px] sm:pr-[40px] pe-0">
+        <p class="md:text-nowrap" data-wow-duration="1s">
+          &copy; 2024 Squaremetres. All rights reserved
+        </p>
       </div>
     </div>
   </div>
-</section>
+</div>
 
 <style>
 </style>
--- src/components/FooterTop.svelte
+++ src/components/FooterTop.svelte
@@ -56,123 +56,127 @@
   }
 </script>
 
-<section class="footer-top md:text-base lg:text-lg text-sm">
-  <div class="container text-green p-2 p-xl-5 p-lg-3">
-    <div class="row mx-0">
-      <div class="col-md-3 col-4 mt-5">
-        <FooterText
-          FooterHead="What We Do"
-          FooterItem={[
-            { href: "/fitila-signature", text: "Fitila Signatures" },
-            { href: "/investment", text: "Investment" },
-            { href: "/agency", text: "Agency" },
-          ]}
-        />
+<div
+  class="container-fluid text-green p-2 p-xl-5 p-lg-3 md:text-base lg:text-lg text-sm xl:w-[100%] lg:w-[94%] w-[95%] mx-auto"
+>
+  <div class="row mx-0">
+    <div class="col-md-3 col-4 mt-5">
+      <FooterText
+        FooterHead="What We Do"
+        FooterItem={[
+          { href: "/fitila-signature", text: "Fitila Signatures" },
+          { href: "/investment", text: "Investment" },
+          { href: "/agency", text: "Agency" },
+        ]}
+      />
+    </div>
+    <div class="col-md-2 col-lg-3 col-4 mt-5">
+      <FooterText
+        FooterHead="Who We Are"
+        FooterItem={[
+          { href: "/about-us", text: "About Us" },
+          { href: "/career", text: "Career" },
+          { href: "/blog", text: "Blog" },
+        ]}
+      />
+    </div>
+    <div class="col-md-3 col-4 mt-5">
+      <FooterText
+        FooterHead="Helpful Links"
+        FooterItem={[
+          { href: "/policy", text: "Terms of service" },
+          { href: "/policy", text: "Policy service" },
+          { href: "/faq", text: "FAQ’s" },
+        ]}
+      />
+    </div>
+    <div class="col-md-4 col-lg-3 col-sm-12 col-12 mt-5">
+      <FooterText
+        FooterHead="Contact Us"
+        FooterItem={[
+          {
+            href: "https://www.google.com/maps/search/?api=1&query=7/9+Molade+Okoya+Thomas,+Victoria+Island,+Lagos",
+            icon: "bi-building-fill",
+            text2: "7/9 Molade Okoya Thomas, Victoria Island, Lagos",
+          },
+          {
+            href: "mailto:hello@squaremetres.ng",
+            icon: "bi-envelope-fill",
+            text2: "hello@squaremetres.ng",
+          },
+          {
+            href: "tel:+2348066267094",
+            icon: "bi-telephone-fill",
+            text2: "08066267094",
+          },
+        ]}
+      />
+    </div>
+  </div>
+</div>
+
+<div
+  class="container-fluid bg-[#F6FFFD] md:text-base lg:text-lg text-sm w-[90%] mx-auto"
+>
+  <div class="row mx-0">
+    <div class="col-3 col-sm-4 mx-auto col-md-3 lg:mt-3 md:mt-10 mt-[38px]">
+      <img
+        src="/images/sqm-footer-logo.png"
+        class="align-baseline"
+        alt="logo"
+      />
+    </div>
+    <div class="col-9 col-sm-8 col-md-5 xl:mt-[74px] lg:mt-[76px] md:mt-[90px] sm:mt-14 mt-12">
+      <div
+        class="content text-[#0d493d] text-nowrap xl:text-lg lg:text-lg md:text-sm sm:text-sm text-[10px] md:text-left text-end lg:leading-6 sm:leading-5 leading-3"
+      >
+        Join more than 1500 recipients of real <br /> estate updates and trends
       </div>
-      <div class="col-md-2 col-lg-3 col-4 mt-5">
-        <FooterText
-          FooterHead="Who We Are"
-          FooterItem={[
-            { href: "/about-us", text: "About Us" },
-            { href: "/career", text: "Career" },
-            { href: "/blog", text: "Blog" },
-          ]}
-        />
-      </div>
-      <div class="col-md-3 col-4 mt-5">
-        <FooterText
-          FooterHead="Helpful Links"
-          FooterItem={[
-            { href: "/policy", text: "Terms of service" },
-            { href: "/policy", text: "Policy service" },
-            { href: "/faq", text: "FAQ’s" },
-          ]}
-        />
-      </div>
-      <div class="col-md-4 col-lg-3 col-sm-12 col-12 mt-5">
-        <FooterText
-          FooterHead="Contact Us"
-          FooterItem={[
-            {
-              href: "https://www.google.com/maps/search/?api=1&query=7/9+Molade+Okoya+Thomas,+Victoria+Island,+Lagos",
-              icon: "bi-building-fill",
-              text2: "7/9 Molade Okoya Thomas, Victoria Island, Lagos",
-            },
-            {
-              href: "mailto:hello@squaremetres.ng",
-              icon: "bi-envelope-fill",
-              text2: "hello@squaremetres.ng",
-            },
-            {
-              href: "tel:+2348066267094",
-              icon: "bi-telephone-fill",
-              text2: "08066267094",
-            },
-          ]}
-        />
+    </div>
+    <div class="col-12 col-md-4 xl:mt-20 lg:mt-20 md:mt-[90px] pb-4">
+      <div class="md:text-base lg:text-lg text-sm">
+        <form on:submit|preventDefault={handleSubmit}>
+          <!-- Display messages based on form submission state -->
+          {#if formState.success}
+            <div>
+              <p class="success text-[#F96B29]">
+                Thank you for subscribing to our newsletter.
+              </p>
+            </div>
+          {/if}
+          {#if formState.missing}
+            <p class="error text-warning">Please input an email address.</p>
+          {/if}
+          {#if formState.incorrect}
+            <p class="error text-warning fw-bolder">
+              The email address is not valid.
+            </p>
+          {/if}
+          {#if formState.exists}
+            <p class="error text-[#F96B29]">
+              We already have you as a subscriber.
+            </p>
+          {/if}
+          <div class="input-group">
+            <input
+              type="email"
+              name="email"
+              bind:value={formState.email}
+              class="form-control text-xs md:text-sm text-[#0d493d] h-10"
+              placeholder="Type your email address"
+              required
+            />
+            <button
+              type="submit"
+              class="btn bg-[#0d493d] hover:bg-[#F96B29] lg:text-base md:text-sm text-xs text-[#f6fffd]"
+              >SIGN UP</button
+            >
+          </div>
+        </form>
       </div>
     </div>
   </div>
-</section>
-
-<section class="md:text-base lg:text-lg text-sm w-[90%] mx-auto">
-  <div class="container bg-[#F6FFFD]">
-      <div class="row mx-0">
-        <div class="col-4 mx-auto col-md-3">
-            <img
-              src="/images/sqm-footer-logo.png"
-              class="w-[150px] mt-md-3"
-              alt="logo"
-            />
-        </div>
-        <div class="col-7 col-md-4 pt-md-5 pt-3">
-          <div
-            class="content text-[#0d493d] md:text-base lg:text-lg text-sm mt-md-2 mt-3 md:text-left text-end lg:leading-6 leading-5"
-          >
-            Join more than 1500 recipients of real <br> estate updates and trends
-          </div>
-        </div>
-        <div class="col-12 col-md-4 pt-md-5 mt-md-3 pt-0 mb-2">
-          <div class="md:text-base lg:text-lg text-sm">
-            <form on:submit|preventDefault={handleSubmit}>
-              <!-- Display messages based on form submission state -->
-              {#if formState.success}
-                <div>
-                  <p class="success text-[#F96B29]">
-                    Thank you for subscribing to our newsletter.
-                  </p>
-                </div>
-              {/if}
-              {#if formState.missing}
-                <p class="error text-warning">Please input an email address.</p>
-              {/if}
-              {#if formState.incorrect}
-                <p class="error text-warning fw-bolder">The email address is not valid.</p>
-              {/if}
-              {#if formState.exists}
-                <p class="error text-[#F96B29]">We already have you as a subscriber.</p>
-              {/if}
-              <div class="input-group">
-                <input
-                  type="email"
-                  name="email"
-                  bind:value={formState.email}
-                  class="form-control text-xs md:text-sm text-[#0d493d] h-10"
-                  placeholder="Type your email address"
-                  required
-                />
-                <button
-                  type="submit"
-                  class="btn bg-[#0d493d] hover:bg-[#F96B29] text-[#f6fffd]"
-                  >SIGN UP</button
-                >
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-  </div>
-</section>
+</div>
 
 <style>
   .text-green {

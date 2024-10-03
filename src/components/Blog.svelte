12abfa2955bfc839da1b441e7f76344b392e9448
alchemistlowkey <alchemistlowkey@gmail.com>
2024-10-03 18:38:56 +0100

    frontend: Modified blog header tag

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -9,7 +9,7 @@
     <div class="row mx-0">
       <div class="col-8 offset-2 mt-[100px]">
         <h1 class="text-[#0D493D] text-center text-lg md:text-xl lg:text-3xl my-3">Square Meters Blog</h1>
-        <p class="text-center text-[#0D493D] text-sm md:text-base lg:text-lg text-wrap my-3">Get the latest news about the real estate industry and how to invest in real estate business.</p>
+        <h3 class="text-center text-[#0D493D] text-sm md:text-base lg:text-lg text-wrap my-3">Get the latest news about the real estate industry and how to invest in real estate business.</h3>
         <!-- <FormBlog /> -->
       </div>
     </div>

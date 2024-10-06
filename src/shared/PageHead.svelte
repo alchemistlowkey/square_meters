d7a3d925801d761c7b00fbf4aa886a1427aee729
alchemistlowkey <alchemistlowkey@gmail.com>
2024-10-06 18:10:13 +0100

    frontend: SEO compliance for hsts

--- src/shared/PageHead.svelte
+++ src/shared/PageHead.svelte
@@ -9,11 +9,11 @@
 <div class="container">
   <div class="row mx-0 py-16">
     <div class="col-12 col-md-10 offset-md-1 py-10 mx-auto text-center md:text-lg lg:text-xl text-base">
-      <p class="career text-w pb-3">{CardHeader}</p>
-      <h1 class="text-w privacy-data pt-4 md:text-3xl xl:text-5xl lg:text-4xl text-2xl tracking-normal leading-10">
+      <h1 class="career text-w pb-3">{CardHeader}</h1>
+      <h2 class="text-w privacy-data pt-4 md:text-3xl xl:text-5xl lg:text-4xl text-2xl tracking-normal leading-10">
         {CardContent}<span class="text-[#F96B29]">{SpanContent}</span
         >{CardContent2}
-      </h1>
+      </h2>
       <p class="text-w p-content md:text-lg lg:text-xl text-base pb-4 pt-3">
         {CardContent3}
       </p>
